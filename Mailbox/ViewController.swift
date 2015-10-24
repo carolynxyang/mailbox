eee0b3ce725e2195ad128bcdc57c171d8d159f14
Carolyn Yang <carolyn.x.yang@gmail.com>
2015-10-24 13:20:33 -0700

    Making some changes and fix scroll problems

--- Mailbox/ViewController.swift
+++ Mailbox/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var scrollView: UIScrollView!
-
+    
     @IBOutlet weak var messageView: UIView!
     @IBOutlet weak var rightBlock: UIImageView!
     @IBOutlet weak var leftBlock: UIImageView!
@@ -39,17 +39,37 @@ class ViewController: UIViewController {
         rescheduleView.alpha = 0
         listView.alpha = 0
         dismissButton.enabled = false
-        self.messageView.center = self.messageStartLocation
+        animateMessage(nil, leftIcon:nil, rightColor:nil, rightIcon:nil, x:self.messageStartLocation.x)
+        
+    }
+    
+    func animateMessage(leftColor:UIColor?, leftIcon:String?, rightColor:UIColor?, rightIcon:String?, x:CGFloat?){
+        UIView.animateWithDuration(0.5, animations: {
+            if(rightColor != nil){
+                self.rightBlock.backgroundColor = rightColor
+            }
+            
+            if(rightIcon != nil){
+                self.rightBlock.image = UIImage(named: rightIcon!)
+            }
+            
+            if(leftColor != nil){
+                self.leftBlock.backgroundColor = leftColor
+            }
+            
+            if(leftIcon != nil){
+                self.leftBlock.image = UIImage(named: leftIcon!)
+            }
+            if(x != nil){
+                self.messageView.center.x = x!
+            }
+            
+        })
     }
     
     @IBAction func messagePanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
-
-        // Absolute (x,y) coordinates in parent view
-        //var point = panGestureRecognizer.locationInView(view)
         
-        // Relative change in (x,y) coordinates from where gesture began.
         let translation = panGestureRecognizer.translationInView(view)
-        //var velocity = panGestureRecognizer.velocityInView(view)
         
         if panGestureRecognizer.state == UIGestureRecognizerState.Began {
             messageLocation = messageView.center
@@ -58,88 +78,51 @@ class ViewController: UIViewController {
             //
             let positionDiff = messageStartLocation.x-messageView.center.x
             print(positionDiff)
-            if(positionDiff<60 && positionDiff>0){
-                rightBlock.image = UIImage(named: "later_icon")
-                UIView.animateWithDuration(0.2, animations: {
-                    self.rightBlock.backgroundColor = UIColor.lightGrayColor()
-                })
-            }else if(positionDiff<260 && positionDiff>60){
-                rightBlock.image = UIImage(named: "later_icon")
-                UIView.animateWithDuration(0.2, animations: {
-                    self.rightBlock.backgroundColor = UIColor.orangeColor()
-                })
-            }else if(positionDiff>260){
-                rightBlock.image = UIImage(named: "list_icon")
-                UIView.animateWithDuration(0.2, animations: {
-                    self.rightBlock.backgroundColor = UIColor.brownColor()
-                })
-            }else if(positionDiff<0 && positionDiff > (-60)){
-             //Gray
-                UIView.animateWithDuration(0.2, animations: {
-                    self.leftBlock.backgroundColor = UIColor.lightGrayColor()
-                })
-            }else if(positionDiff < (-60) && positionDiff > (-260)){
+            if(positionDiff < 60 && positionDiff > -60){
+                animateMessage(UIColor.lightGrayColor(), leftIcon:"archive_icon", rightColor:UIColor.lightGrayColor(), rightIcon:"later_icon", x:nil)
+            }else if(positionDiff < 260 && positionDiff > 60){
+                animateMessage(nil, leftIcon:nil, rightColor:UIColor.orangeColor(), rightIcon:"later_icon", x:nil)
+            }else if(positionDiff > 260){
+                animateMessage(nil, leftIcon:nil, rightColor:UIColor.brownColor(), rightIcon:"list_icon", x:nil)
+            }else if(positionDiff < -60 && positionDiff > -260){
                 //Green
-                UIView.animateWithDuration(0.2, animations: {
-                    self.leftBlock.backgroundColor = UIColor.greenColor()
-                })
-
-            }else if(positionDiff < (-260)){
-                //Red
-                UIView.animateWithDuration(0.2, animations: {
-                    self.leftBlock.backgroundColor = UIColor.redColor()
-                })
-
+                animateMessage(UIColor.greenColor(), leftIcon:"archive_icon", rightColor:nil, rightIcon:nil, x:nil)
+            }else if(positionDiff < -260){
+                animateMessage(UIColor.redColor(), leftIcon:"delete_icon", rightColor:nil, rightIcon:nil, x:nil)
             }
         } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
-
- 
+            
             let positionDiff = messageStartLocation.x-messageView.center.x
             
-            if(positionDiff<60 && positionDiff > -60){
+            if(positionDiff < 60 && positionDiff > -60){
                 //Snap back
-                UIView.animateWithDuration(0.2, animations: {
-                    self.messageView.center = self.messageStartLocation
-                })
-            }else if(positionDiff>60 && positionDiff<260){
+                animateMessage(nil, leftIcon:nil, rightColor:nil, rightIcon:nil, x:self.messageStartLocation.x)
+            }else if(positionDiff > 60 && positionDiff < 260){
                 // Snap to yellow
-                UIView.animateWithDuration(0.2, animations: {
-                    self.messageView.center.x = self.messageStartLocation.x-320
-                    self.rescheduleView.alpha = 1.0
-                })
+                animateMessage(nil, leftIcon:nil, rightColor:nil, rightIcon:nil, x:self.messageStartLocation.x-320)
+                self.rescheduleView.alpha = 1.0
                 dismissButton.enabled = true
-            }else if(positionDiff>260 && positionDiff<320){
-                UIView.animateWithDuration(0.2, animations: {
-                    self.messageView.center.x = self.messageStartLocation.x-320
-                    self.listView.alpha = 1.0
-                })
+            }else if(positionDiff > 260 && positionDiff < 320){
+                animateMessage(nil, leftIcon:nil, rightColor:nil, rightIcon:nil, x:self.messageStartLocation.x-320)
+                self.listView.alpha = 1.0
                 dismissButton.enabled = true
-            }else if (positionDiff>320 || positionDiff < (-320)){
-                UIView.animateWithDuration(0.2, animations: {
-                    self.messageView.center = self.messageStartLocation
-                })
-            }else if (positionDiff < (-60) && positionDiff > (-260)){
-                UIView.animateWithDuration(0.2, animations: {
-                    self.messageView.center.x = self.messageStartLocation.x+320
-                },
-                    completion:{
-                        (value: Bool) in
-                        self.messageView.alpha = 0
-                        self.scrollView.contentOffset.y+=86
-                })
-                
-            }else if(positionDiff < (-260)){
+            }else if (positionDiff > 320 || positionDiff < -320){
+                animateMessage(nil, leftIcon:nil, rightColor:nil, rightIcon:nil, x:self.messageStartLocation.x)
+            }else if (positionDiff < -60){
                 UIView.animateWithDuration(0.2, animations: {
                     self.messageView.center.x = self.messageStartLocation.x+320
                     },
                     completion:{
                         (value: Bool) in
-                        self.messageView.alpha = 0
-                        self.scrollView.contentOffset.y+=86
+                        UIView.animateWithDuration(0.2, animations: {
+                            self.messageView.alpha = 0
+                            self.scrollView.contentInset.top-=86
+                        })
                 })
+                
             }
         }
-
+        
     }
     
     @IBAction func mainMenuPan(panGestureRecognizer: UIPanGestureRecognizer) {
@@ -154,7 +137,7 @@ class ViewController: UIViewController {
                 UIView.animateWithDuration(0.5, animations: {
                     self.mainView.center.x = 340
                 })
-
+                
             }else{
                 UIView.animateWithDuration(0.5, animations: {
                     self.mainView.center.x = 600
@@ -168,6 +151,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
